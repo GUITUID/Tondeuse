fab4dcdff2e550e79920281a544ecbbc3cdfef26
zadwarf <zadwa@DESKTOP-13VUVSE>
2017-09-17 21:54:06 +0200

    MonsieurZad - Modifications on Exceptions

--- ExerciceTondeuse/src/helpersPack/LireFichier.java
+++ ExerciceTondeuse/src/helpersPack/LireFichier.java
@@ -29,30 +29,57 @@ class NombreInstructionsGrilleExceptions extends Exception{
  * On peut ajouter ces exceptions personnalisées, pour des raison esthetiques, pour le traitement du fichier cependant ces cas renvoient déjà une exception qui arrète le programme
  *
  **/
-/*
-class NombreInstructionsGrilleNonIntExceptions extends Exception{ 
-	  public NombreInstructionsGrilleNonIntExceptions(){
-	    System.out.println("Les instructions pour les coordonnées de la grille ne sont pas des chiffres.");
+
+class InstructionsNonIntExceptions extends Exception{ 
+	  public InstructionsNonIntExceptions(){
+	    System.out.println("Les instructions pour les coordonnées ne sont pas des nombres.");
 
 	  }  
 	}
 
-class NombreInstructionsTondeusesCoordonneesInvalidesExceptions extends Exception{ 
-	  public NombreInstructionsTondeusesCoordonneesInvalidesExceptions(){
+class InstructionsTondeusesCoordonneesInvalidesExceptions extends Exception{ 
+	  public InstructionsTondeusesCoordonneesInvalidesExceptions(){
 	    System.out.println("Les instructions pour les coordonnées des tondeuses sont invalides.");
 
 	  }  
 	}
 
-class NombreInstructionsTondeusesInvalidesExceptions extends Exception{ 
-	  public NombreInstructionsTondeusesInvalidesExceptions(){
+class InstructionsTondeusesInvalidesExceptions extends Exception{ 
+	  public InstructionsTondeusesInvalidesExceptions(){
 	    System.out.println("Les instructions pour le déplacement des tondeuses sont invalides.");
 
 	  }  
 	}
-*/
+
+// Ne survient pas le test sur la parité des instructions discrimine ce test
+class NombreNegatifExceptions extends Exception{ 
+	  public NombreNegatifExceptions(){
+	    System.out.println("Les instructions sont invalides (valeurs négatives).");
+	  }  
+}
+
 
 public class LireFichier {
+	
+	public static boolean isNombre(String valeur) throws InstructionsNonIntExceptions{
+	    boolean parsable = false;
+	    try{
+	        Integer.parseInt(valeur);
+	        parsable = true;
+	    }catch(NumberFormatException e){
+	    	throw new InstructionsNonIntExceptions();
+	    }
+		return parsable;
+	}
+	
+	public static boolean isPositif(int valeur) throws NombreNegatifExceptions{
+	    if(valeur>0){
+	    	return true;
+	    }
+	    else{
+	    	throw new NombreNegatifExceptions();
+	    }
+	}
 
 	static int nombreTondeuses;
 	static List<Tondeuse> tondeuses = new ArrayList<Tondeuse>();
@@ -106,9 +133,20 @@ public class LireFichier {
 			String [] casesJardin = listeInstructions.get(0).split(" ");
 			// On teste si le nombre de coordonnées de la grille est pair
 			if(casesJardin.length%2==0){
-				
+			
+			// Vérifie si la valeur est un nombre non négatif
+			if(isNombre(casesJardin[0])){
+				if(isPositif(Integer.parseInt(casesJardin[0]))){
+				}
+			}
+			if(isNombre(casesJardin[1])){
+				if(isPositif(Integer.parseInt(casesJardin[1]))){
+				}
+			}
 			int casesJardinX = Integer.parseInt(casesJardin[0]);
 			int casesJardinY = Integer.parseInt(casesJardin[1]);
+			
+			isNombre(casesJardin[0]);
 			
 			Jardin.InitSizeJardin(casesJardinX,casesJardinY);
 		
@@ -120,6 +158,17 @@ public class LireFichier {
 						String[] positionsTondeuse=listeInstructions.get(premiereTondeuse-1).split(" ");
 						String[] instructionsTondeuse=listeInstructions.get(premiereTondeuse).split("");
 						Tondeuse tondeuse = new Tondeuse();
+						
+						// Vérifie si la valeur est un nombre non négatif
+						if(isNombre(positionsTondeuse[0])){
+							if(isPositif(Integer.parseInt(positionsTondeuse[0]))){
+							}
+						}
+						if(isNombre(positionsTondeuse[1])){
+							if(isPositif(Integer.parseInt(positionsTondeuse[1]))){
+							}
+						}
+						
 						// On créé un objet Tondeuse qu'on ajoute dans la liste des tondeuses
 						tondeuse.tondeuseConstructor(i,Integer.parseInt(positionsTondeuse[0]),Integer.parseInt(positionsTondeuse[1]),positionsTondeuse[2]);
 						//tondeuse.getStatutTondeuse();
@@ -132,6 +181,9 @@ public class LireFichier {
 							// On traite les instructions de la tondeuse
 							for(int y = 0 ;y!=instructionsTondeuse.length;y++){
 								// System.out.println("instruction: " +instructionsTondeuse[y]);
+								// Vérifie si l'instruction est bien un String
+								
+								
 								tondeuses.get(i).instructionTondeuse(instructionsTondeuse[y],tondeuses.get(i).getTondeuseOrientation(),tondeuses.get(i).getPosXTondeuse(),tondeuses.get(i).getPosYTondeuse());
 								//tondeuses.get(i).getStatutTondeuse();
 							}
